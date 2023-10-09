18ab9d28e488ec6bf61a979cbd62bb238d7e8afd
Sreenivasnaru <Sreenunaru672@gmail.com>
2023-10-09 20:45:24 +0530

    full stack project

---
+++ models/listing.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const listingSchema = new Schema({
+    title: {
+        type: String,
+        required: true,
+    },
+    description: String,
+    image: {
+        type: String,
+        default: "https://unsplash.com/photos/1Z2niiBPg5A",
+        set: (v) => v ==="" ? "https://unsplash.com/photos/1Z2niiBPg5A" : v,
+    },
+    price: Number,
+    location: String,
+    country: String,
+});
+
+const Listing = mongoose.model("Listing", listingSchema);
+module.exports = Listing;
