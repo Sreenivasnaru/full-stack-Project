f7fd1514ff13a53862be5a972f671138ab554711
Sreenivasnaru <Sreenunaru672@gmail.com>
2023-10-21 16:08:49 +0530

    added authentication

--- routes/listing.js
+++ routes/listing.js
@@ -32,6 +32,10 @@ router.get("/new", (req, res) => {
 router.get("/:id", wrapAsync(async(req,res) => {
     let{id} = req.params;
     const listing = await Listing.findById(id).populate("reviews"); 
+    if(!listing) {
+        req.flash("error", "Listing you requested for does not exist!");
+        res.redirect("/listings");
+    }
     res.render("listings/show.ejs", {listing});
 } ));
 
@@ -47,6 +51,10 @@ router.post("/", validateListing,wrapAsync(async(req, res, next) => {
 router.get("/:id/edit", wrapAsync(async (req, res) => {
     let{id} = req.params;
     const listing = await Listing.findById(id);
+    if(!listing) {
+        req.flash("error", "Listing you requested for does not exist!");
+        res.redirect("/listings");
+    }
     res.render("listings/edit.ejs", {listing});
 }));
 
@@ -54,6 +62,7 @@ router.get("/:id/edit", wrapAsync(async (req, res) => {
 router.put("/:id", validateListing,wrapAsync(async (req,res) => {
     let {id} =req.params;
     await Listing.findByIdAndUpdate(id, {...req.body.listing});
+    req.flash("success", "Listing Updated!");
     res.redirect(`/listings/${id}`);
 }));
 
@@ -61,7 +70,7 @@ router.put("/:id", validateListing,wrapAsync(async (req,res) => {
 router.delete("/:id", wrapAsync(async(req, res) => {
     let {id} = req.params;
     let deletedListing = await Listing.findByIdAndDelete(id);
-    console.log(deletedListing);
+    req.flash("success", "Listing Deleted!");
     res.redirect("/listings");
 }));
 
