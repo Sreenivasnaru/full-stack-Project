71092dab43df9d4a04eca3b109e79dfc661655d4
Sreenivasnaru <Sreenunaru672@gmail.com>
2023-10-24 15:07:23 +0530

    Added Star Review

--- routes/listing.js
+++ routes/listing.js
@@ -8,10 +8,11 @@ const listingController = require("../controllers/listings.js");
 
 router.route("/").get( wrapAsync(listingController.index)).post(isLoggedIn, validateListing,wrapAsync(listingController.createListing));
 
-router.route("/:id").get(wrapAsync(listingController.showListing)).put(isLoggedIn,isOwner,  validateListing,wrapAsync(listingController.updateListing)).delete(isLoggedIn,isOwner, wrapAsync(listingController.destroyListing));
-
 //New Route
 router.get("/new", isLoggedIn, listingController.renderNewForm);
+
+router.route("/:id").get(wrapAsync(listingController.showListing)).put(isLoggedIn,isOwner,  validateListing,wrapAsync(listingController.updateListing)).delete(isLoggedIn,isOwner, wrapAsync(listingController.destroyListing));
+
 
 //Edit Route
 router.get("/:id/edit", isLoggedIn,isOwner, wrapAsync(listingController.renderEditForm));
