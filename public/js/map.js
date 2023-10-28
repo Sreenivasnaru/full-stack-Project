f68cb11d67e4ed95723dd9df9437bfca33d9c70f
Sreenivasnaru <Sreenunaru672@gmail.com>
2023-10-28 12:40:55 +0530

    Final deploying project

--- public/js/map.js
+++ public/js/map.js
@@ -11,7 +11,7 @@ const map = new mapboxgl.Map({
 const marker1 = new mapboxgl.Marker({color: "red"})
   .setLngLat(listing.geometry.coordinates) //Listing.geometry.coordinates
   .setPopup(new mapboxgl.Popup({ offset: 25  })
-  .setHTML(`<h4>${listing.location}</h4><p>Exact location will be provided after booking</p>`))
+  .setHTML(`<h4>${listing.title}</h4><p>Exact location will be provided after booking</p>`))
   .addTo(map);
 
   
